367d9d04d496bddd56ec5606b0af445e3b52cdd6
Sunpy <sunpy@osufx.com>
2017-12-04 10:16:30 +0100

    Use settings

--- js/art-canvas.js
+++ js/art-canvas.js
@@ -1,25 +1,26 @@
-function initArtCanvas(canvas = null){
-    if(canvas == null){
-        var artCanvasList = document.getElementsByClassName("art-canvas");
+function initArtCanvas(){
+    var artCanvasList = document.getElementsByClassName("art-canvas");
 
-        for(var i = 0; i < artCanvasList.length; i++){
-            switch(artCanvasList[i].nodeName){
-                case "CANVAS":
-                    initArtCanvas(artCanvasList[i]);
-                    break;
-                case "BUTTON":
-                    initArtButton(artCanvasList[i]);
-                    break;
-                default:
-                    console.error(artCanvasList[i] + " is not a valid art-canvas element! \n[Valid types are canvas & button]");
-                    break;
-            }
+    for(var i = 0; i < artCanvasList.length; i++){
+        switch(artCanvasList[i].nodeName){
+            case "CANVAS":
+                if (typeof settings !== 'undefined'){
+                    if (settings.hasOwnProperty("generateArtCanvasOnSpawn"))
+                        if (settings["generateArtCanvasOnSpawn"])
+                            generateArtCanvas(artCanvasList[i]);
+                    else
+                        console.error("settings does not have generateArtCanvasOnSpawn data set");
+                }else
+                    console.log("No settings found, not running generateArtCanvas by default.");
+                break;
+            case "BUTTON":
+                initArtButton(artCanvasList[i]);
+                break;
+            default:
+                console.error(artCanvasList[i] + " is not a valid art-canvas element! \n[Valid types are canvas & button]");
+                break;
         }
-
-        return;
     }
-
-    canvas
 }
 
 function initArtButton(button){
